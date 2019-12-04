9de8486b70cdeea7f3eed4d5842b105568e7b4ea
chepkiruibetty <chepkiruibetty64@gmail.com>
2019-12-04 13:29:49 +0300

    introduced evaluate function

--- js/script.js
+++ js/script.js
@@ -1,16 +1,5 @@
-var add = function(number1, number2) {
-  return number1 + number2;
-};
-var subtract = function(number1, number2) {
-  return number1 - number2;
-};
-var multiply = function(number1, number2) {
-  return number1 * number2;
-};
-var divide = function(number1, number2) {
-  return number1 / number2;
-};
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-alert(add(number1, number2));
+function res (){
+var digits =document.getElementById("add1").value;
+var results = eval(digits);
+ document.getElementById("ans").innerHTML=results;
+}
